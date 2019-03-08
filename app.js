813d2d822f73b8a1f0a5021d1e6f1e92747f5fd1
Jeff Killian <jeff.killian@atscale.com>
2019-03-07 20:08:45 -0800

    All working

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 const cors = require('cors');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');// to parse a post request
-
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 app.options('*', cors());
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 9001
  // don't have connection settings in here because we push// var connection = mysql.createConnection(connectionSettings);
 
 let globalVar = 1
@@ -27,32 +29,16 @@ app.get('/increment', function (req, res) {
   res.send(`Global var is ${globalVar++}`);
 });
 
-app.post('/postScore', function (req, res) {
-  let score = req.body.score
-  connection.query("insert into scores (score) values (?)", score, function (err, result) {
-    if (err) {
-      res.send(err);
-      return 
-    }
-    res.send("Successfully ran query");
-  });
-});
-
-app.get('/highScores', function (req, res) {
-  connection.query("SELECT score FROM `scores` ORDER BY score LIMIT 10", function (err, result) {
-    if (err) {
-      res.send(err);
-      return 
-    }
-    res.send(result);
-  });
+app.get('/randomNumber', function (req, res) {
+  let random = Math.random()
+  res.send({number:random})
 });
 
 app.get('/testEndpoint', function (req, res) {
-  res.send("YOU DID IT244")
+  res.send("YOU DID IT jeff")
 });
 
 
 app.listen(PORT, function () {
-  console.log('Node server started listening on port 6001');
+  console.log(`Node server started listening on port ${PORT}`);
 });
